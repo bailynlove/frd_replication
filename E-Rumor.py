afff4a95804723e159f9326c6c85ae57e72814dc
bailynlove <knightrr9@gmail.com>
2025-08-08 18:39:16 +0800

    fix(E-Rumor): import

--- E-Rumor.py
+++ E-Rumor.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-from transformers import BertModel, BertTokenizer, AdamW
+from transformers import BertModel, BertTokenizer
+from torch.optim import AdamW
 from torch.distributions.multivariate_normal import MultivariateNormal
 import pandas as pd
 import numpy as np
